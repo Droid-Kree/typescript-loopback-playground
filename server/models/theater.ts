85e3c4fc7af0a4c4ed987fd2bc6a425f7bd50b82
Droid-Kree <droidkree@gmail.com>
2016-04-09 10:56:21 +0300

    Theater list endpoint with 'mock' data, connected MongoDb

--- server/models/theater.ts
+++ server/models/theater.ts
@@ -1,3 +1,17 @@
 module.exports = function(Theater) {
+  Theater.list = function (cb) {
+    var theaters = [];
+    var filter = {};
+    Theater.find(filter, function (err, theater) {
+      console.log('theater: ', theater);
+      theaters.push(theater);
+    });
+    console.log('theaters: ', theaters);
+    cb(null, theaters);
+  };
 
+  Theater.remoteMethod('list', {
+    http: {path: '/list', verb: 'get'},
+    returns: {arg: 'list', type: 'string'}
+  })
 };
