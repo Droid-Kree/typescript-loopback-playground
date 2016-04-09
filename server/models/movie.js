e89060284fb67930c042866baee7520df205a72a
Droid-Kree <droidkree@gmail.com>
2016-04-09 17:29:06 +0300

    Movie remote methods: create, single, update

--- server/models/movie.js
+++ server/models/movie.js
@@ -1,4 +1,13 @@
 module.exports = function (Movie) {
+    Movie.create = function (id, title, cb) {
+        var movie = {
+            "data": {
+                "id": id,
+                "title": title
+            }
+        };
+        cb(null, movie);
+    };
     Movie.list = function (cb) {
         var movies = [
             { title: 'Movie 0' },
@@ -7,9 +16,40 @@ module.exports = function (Movie) {
         ];
         cb(null, movies);
     };
+    Movie.single = function (id, title, cb) {
+        var single = {
+            "data": {
+                "id": id,
+                "title": title
+            }
+        };
+        cb(null, single);
+    };
+    Movie.update = function (id, title, cb) {
+        var update = {
+            "data": {
+                "id": id,
+                "title": title
+            }
+        };
+        cb(null, update);
+    };
+    Movie.remoteMethod('create', {
+        accepts: [{ arg: 'id', type: 'number' }, { arg: 'title', type: 'string' }],
+        http: { path: '/create' },
+        returns: { arg: 'create', type: 'string' }
+    });
     Movie.remoteMethod('list', {
         http: { path: '/list', verb: 'get' },
         returns: { arg: 'list', type: 'string' }
+    });
+    Movie.remoteMethod('single', {
+        http: { path: '/single', verb: 'get' },
+        returns: { arg: 'single', type: 'string' }
+    });
+    Movie.remoteMethod('update', {
+        http: { path: '/update', verb: 'put' },
+        returns: { arg: 'update', type: 'string' }
     });
 };
 //# sourceMappingURL=movie.js.map
--- server/models/movie.ts
+++ server/models/movie.ts
@@ -1,4 +1,14 @@
 module.exports = function(Movie:any) {
+  Movie.create = function (id, title, cb) {
+    let movie = {
+      "data": {
+        "id": id,
+        "title": title
+      }
+    };
+    cb(null, movie);
+  };
+
   Movie.list = function(cb) {
     let movies = [
       { title: 'Movie 0' },
@@ -8,8 +18,44 @@ module.exports = function(Movie:any) {
     cb(null, movies);
   };
 
+  Movie.single = function (id, title, cb) {
+    let single = {
+      "data": {
+        "id": id,
+        "title": title
+      }
+    };
+    cb(null, single);
+  };
+
+  Movie.update = function (id, title, cb) {
+    let update = {
+      "data": {
+        "id": id,
+        "title": title
+      }
+    };
+    cb(null, update);
+  };
+
+  Movie.remoteMethod('create', {
+    accepts: [{arg:'id', type:'number'}, {arg:'title', type:'string'}],
+    http: {path: '/create'},
+    returns: {arg: 'create', type: 'string'}
+  });
+
   Movie.remoteMethod('list', {
     http: {path: '/list', verb: 'get'},
     returns: {arg: 'list', type: 'string'}
+  });
+
+  Movie.remoteMethod('single', {
+    http: {path: '/single', verb: 'get'},
+    returns: {arg: 'single', type: 'string'}
+  });
+
+  Movie.remoteMethod('update', {
+    http: {path: '/update', verb: 'put'},
+    returns: {arg:'update', type:'string'}
   });
 };
